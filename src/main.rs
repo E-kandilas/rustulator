bb4421e645398af28c17b893afecccbbcd0fa2d4
Emmanuel Kandilas <ekandilas@gmail.com>
2024-09-04 18:53:33 -0400

    putting a pin in it until next time

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+#[allow(dead_code)]
 
 use fltk::{prelude::*, button::Button, enums::{Color, FrameType}, app, *};
 use fltk::enums::Event;
-// use fltk::app::{Sender, Receiver}; // Import Sender and Receiver
 use frame::Frame;
 use rand::Rng;
-// use fltk::app::channel; // Import channel
 
 use self::Message::*;
 
@@ -12,11 +11,54 @@ use self::Message::*;
 // Define the Message type
 #[derive(Debug, Clone, Copy)]
 enum Message {
-  Spaz,
-  Num0, Num1, Num2, Num3, Num4,
-  Num5, Num6, Num7, Num8, Num9,
-  Back, Mult, Div, 
+    Spaz,
+    Num0, Num1, Num2, Num3, Num4,
+    Num5, Num6, Num7, Num8, Num9,
+    Back, Mult, Div,
 }
+
+impl std::fmt::Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Message::Spaz => write!(f, "Spaz"),
+            Message::Num0 => write!(f, "0"),
+            Message::Num1 => write!(f, "1"),
+            Message::Num2 => write!(f, "2"),
+            Message::Num3 => write!(f, "3"),
+            Message::Num4 => write!(f, "4"),
+            Message::Num5 => write!(f, "5"),
+            Message::Num6 => write!(f, "6"),
+            Message::Num7 => write!(f, "7"),
+            Message::Num8 => write!(f, "8"),
+            Message::Num9 => write!(f, "9"),
+            Message::Back => write!(f, "Back"),
+            Message::Mult => write!(f, "Mult"),
+            Message::Div => write!(f, "Div"),
+        }
+    }
+}
+
+// impl Message {
+//     pub fn iterator() -> std::slice::Iter<'static, Message> {
+//         static MESSAGES: [Message; 14] = [
+//             Message::Spaz,
+//             Message::Num0,
+//             Message::Num1,
+//             Message::Num2,
+//             Message::Num3,
+//             Message::Num4,
+//             Message::Num5,
+//             Message::Num6,
+//             Message::Num7,
+//             Message::Num8,
+//             Message::Num9,
+//             Message::Back,
+//             Message::Mult,
+//             Message::Div,
+//         ];
+//         MESSAGES.iter()
+//     }
+// }
 
 impl Message {
   pub fn iterator() -> std::slice::Iter<'static, Message> {
@@ -37,46 +79,49 @@ fn main() {
 
     let (sender, reciever) = app::channel::<Message>();
 
+    let mut buttCount = 0;
     for buttonType in Message::iterator() {
-      println!("{:?}", buttonType)
+      let mut buttName = buttCount.to_string();
+      let buttDist = buttCount * 160;
+      if buttCount <= 9 {
+          buttName = buttCount.to_string();
+      } else {
+          buttName = buttonType.to_string();
+      }
+      let mut but = Button::new(160, 200+buttDist, 80, 40,&*buttName );
+      buttCount = buttCount + 1;
     }
 
 
-    let mut frame = Frame::new(0, 0, 400, 300, "");
-    frame.set_color(fltk::enums::Color::White);
-    let mut but = Button::new(160, 200, 80, 40, "Click me!");
-    but.set_color(Color::Cyan);
-    but.set_label_color(Color::Red);
-    but.set_frame(FrameType::FlatBox);
-    but.set_label_size(20);
-    // but.set_callback(move |_| frame.set_label("Hello World!"));
+    // let mut frame = Frame::new(0, 0, 400, 300, "");
+    // frame.set_color(fltk::enums::Color::White);
+    // let mut but = Button::new(160, 200, 80, 40, "Click me!");
+    // but.set_color(Color::Cyan);
+    // but.set_label_color(Color::Red);
+    // but.set_frame(FrameType::FlatBox);
+    // but.set_label_size(20);
 
-    but.handle(move |b, evt| match evt {
-        Event::Enter => {
-          b.set_color(Color::XtermBgYellow);
-          b.redraw();
-          return true;
-        }
-        Event::Leave => {
-          b.set_color(Color::Cyan);
-          b.redraw();
-          return true;
-        }
-        // using resize changes the behavior of the button and thus needs different behavior
-        Event::Push => {
-            // let rand_x = rand::thread_rng().gen_range(0..(400 - 80));
-            // let rand_y = rand::thread_rng().gen_range(0..100);
-            // but.resize(rand_x, rand_y, 80, 40);
-            // but.redraw();
-            // return true;
-            b.emit(sender, Message::Spaz);
-            return true;
-        }
-        _ => {
-          // print!("{:?}", evt);
-          return false;
-        },
-    });
+    // but.handle(move |b, evt| match evt {
+    //     Event::Enter => {
+    //       b.set_color(Color::XtermBgYellow);
+    //       b.redraw();
+    //       return true;
+    //     }
+    //     Event::Leave => {
+    //       b.set_color(Color::Cyan);
+    //       b.redraw();
+    //       return true;
+    //     }
+    //     // using resize changes the behavior of the button and thus needs different behavior
+    //     Event::Push => {
+    //         b.emit(sender, Message::Spaz);
+    //         return true;
+    //     }
+    //     _ => {
+    //       // print!("{:?}", evt);
+    //       return false;
+    //     },
+    // });
     
     
     win.end();
@@ -87,20 +132,17 @@ fn main() {
       // Look into if lets again and is there another way / what is this syntactic sugar for?
       if let Some(msg) = reciever.recv() {
         match msg {
-          Message::Spaz => {
-            let rand_x = rand::thread_rng().gen_range(0..(400 - 80));
-            let rand_y = rand::thread_rng().gen_range(0..100);
-            but.resize(rand_x, rand_y, 80, 40);
-            win.redraw();
-          }
+          // Message::Spaz => {
+          //   let rand_x = rand::thread_rng().gen_range(0..(400 - 80));
+          //   let rand_y = rand::thread_rng().gen_range(0..100);
+          //   but.resize(rand_x, rand_y, 80, 40);
+          //   win.redraw();
+          // }
           _ => (), // dont need if all enums are handled btw
         }
       }
     }
-    
-    // but.emit
-    
-    
+
     app.run().unwrap();
 }
 
