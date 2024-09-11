4d1751ae6bf31d8d8b51263115e2647084080406
Emmanuel Kandilas <ekandilas@gmail.com>
2024-09-11 19:50:06 -0400

    Another wednesday another rusty experience

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use fltk::{prelude::*, button::Button, enums::{Color, FrameType}, app, *};
 use fltk::enums::Event;
 use frame::Frame;
-use rand::Rng;
+use macros::display;
 
 use self::Message::*;
 
@@ -11,16 +11,14 @@ use self::Message::*;
 // Define the Message type
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    Spaz,
     Num0, Num1, Num2, Num3, Num4,
     Num5, Num6, Num7, Num8, Num9,
-    Back, Mult, Div,
+    CE, Mult, Div, Dec, Eqs
 }
 
 impl std::fmt::Display for Message {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Message::Spaz => write!(f, "Spaz"),
             Message::Num0 => write!(f, "0"),
             Message::Num1 => write!(f, "1"),
             Message::Num2 => write!(f, "2"),
@@ -31,44 +29,30 @@ impl std::fmt::Display for Message {
             Message::Num7 => write!(f, "7"),
             Message::Num8 => write!(f, "8"),
             Message::Num9 => write!(f, "9"),
-            Message::Back => write!(f, "Back"),
+            Message::CE => write!(f, "CE"),
             Message::Mult => write!(f, "Mult"),
             Message::Div => write!(f, "Div"),
+            Message::Dec => write!(f, "."),
+            Message::Eqs => write!(f, "=")
         }
     }
 }
 
-// impl Message {
-//     pub fn iterator() -> std::slice::Iter<'static, Message> {
-//         static MESSAGES: [Message; 14] = [
-//             Message::Spaz,
-//             Message::Num0,
-//             Message::Num1,
-//             Message::Num2,
-//             Message::Num3,
-//             Message::Num4,
-//             Message::Num5,
-//             Message::Num6,
-//             Message::Num7,
-//             Message::Num8,
-//             Message::Num9,
-//             Message::Back,
-//             Message::Mult,
-//             Message::Div,
-//         ];
-//         MESSAGES.iter()
-//     }
-// }
-
 impl Message {
   pub fn iterator() -> std::slice::Iter<'static, Message> {
-      static MESSAGES: [Message; 14] = [Spaz,
+      static MESSAGES: [Message; 15] = [
       Num0, Num1, Num2, Num3, Num4,
       Num5, Num6, Num7, Num8, Num9,
-      Back, Mult, Div];
+      Dec, CE, Mult, Div, Eqs,
+      ];
       MESSAGES.iter()
   }
 }
+// TODO:
+// Add All Calculator buttons %,+, -, Look at an actual calculator
+// Look into ways to change the placements of the buttons
+// Begin storing messages and displaying them on the display_frame
+// Add some more constants and variables for window sizes
 
 fn main() {
     // CONSTANTS
@@ -79,74 +63,46 @@ fn main() {
 
     let app = app::App::default().with_scheme(fltk::app::Scheme::Gtk);
 
-    let mut win = fltk::window::Window::new(100, 100, 400, 300, "Rustulator");
-    let mut frame = fltk::frame::Frame::new(0, 0, 400, 300, "");
-    frame.set_color(fltk::enums::Color::White);
+    let mut win = fltk::window::Window::new(100, 100, 400, 400, "Rustulator").center_screen();
+    // let mut control_frame = fltk::frame::Frame::new(30, 30, 400, 300, "Control");
+    let mut display_frame =  fltk::frame::Frame::new(40, 10, 300, 30, "Display").with_label("Begin Typing");
+
+    display_frame.set_color(Color::from_rgb(255, 255, 255));
+    display_frame.set_frame(FrameType::BorderBox);
+
 
     let (sender, reciever) = app::channel::<Message>();
 
+    // Button Generation
     let mut butt_count = 0;
     let mut butts_per_row = 0;
     let mut y_offset = 0;
-
+    // let butt_body = group::Flex::default(); // This is an idea worth looking into to control button location placement
     for button_type in Message::iterator() {
       butts_per_row += 1;
-      let mut butt_name = butt_count.to_string();
+      let mut butt_name= butt_count.to_string();
       
       if butt_count % 3 == 0 {
-        // butt_dist = 0;
         butts_per_row = 0;
         y_offset = y_offset + BUTT_HEIGHT;
       }
       let butt_dist = EDGE_DIST + (butts_per_row * BUTT_WIDTH);
       
-      if butt_count <= 9 {
-          butt_name = butt_count.to_string();
-      } else {
-          butt_name = button_type.to_string();
+      if butt_count > 9 {
+        butt_name = button_type.to_string();
       }
-      Button::new(butt_dist, y_offset, BUTT_WIDTH, BUTT_HEIGHT,&*butt_name );
+
+      Button::new(butt_dist, y_offset, BUTT_WIDTH, BUTT_HEIGHT,&*butt_name);
       butt_count = butt_count + 1;
     }
 
 
-    // let mut frame = Frame::new(0, 0, 400, 300, "");
-    // frame.set_color(fltk::enums::Color::White);
-    // let mut but = Button::new(160, 200, 80, 40, "Click me!");
-    // but.set_color(Color::Cyan);
-    // but.set_label_color(Color::Red);
-    // but.set_frame(FrameType::FlatBox);
-    // but.set_label_size(20);
-
-    // but.handle(move |b, evt| match evt {
-    //     Event::Enter => {
-    //       b.set_color(Color::XtermBgYellow);
-    //       b.redraw();
-    //       return true;
-    //     }
-    //     Event::Leave => {
-    //       b.set_color(Color::Cyan);
-    //       b.redraw();
-    //       return true;
-    //     }
-    //     // using resize changes the behavior of the button and thus needs different behavior
-    //     Event::Push => {
-    //         b.emit(sender, Message::Spaz);
-    //         return true;
-    //     }
-    //     _ => {
-    //       // print!("{:?}", evt);
-    //       return false;
-    //     },
-    // });
-    
-    
     win.end();
     win.show();
     
     
     while app.wait() {
-      // Look into if lets again and is there another way / what is this syntactic sugar for?
+      //Lesson! Look into if lets again and is there another way / what is this syntactic sugar for?
       if let Some(msg) = reciever.recv() {
         match msg {
           // Message::Spaz => {
