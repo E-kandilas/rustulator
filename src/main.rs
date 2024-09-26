8c0c369114f7ea2378aea0ca05a49bacd7c320f1
Emmanuel Kandilas <ekandilas@gmail.com>
2024-09-25 20:10:38 -0400

    abstrsact event handling for button and keyboard events, handle the maths logic. Its not perfect but its a fine toy

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,24 @@ use self::Message::*;
 #[derive(Debug, Clone, Copy)]
 enum Message {
   Num0, Num1, Num2, Num3, Num4,
-  Num5, Num6, Num7, Num8, Num9, LPar, RPar, Pow, BkSp,
-  CE, Plus, Minus, Mult, Div, Dec, Eqs
+  Num5, Num6, Num7, Num8, Num9, Mod, Pow, BkSp,
+  C, CE, Plus, Minus, Mult, Div, Dec, Eqs
 }
 
 impl std::fmt::Display for Message {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self {
+      Message::CE => write!(f, "CE"),
+      Message::C => write!(f, "C"),
+      Message::Pow => write!(f, "^"),
+      Message::BkSp => write!(f, "Back"),
+      Message::Mod => write!(f, "%"),
+      Message::Div => write!(f, "/"),
+      Message::Plus => write!(f, "+"),
+      Message::Minus => write!(f, "-"),
+      Message::Mult => write!(f, "*"),
+      Message::Dec => write!(f, "."),
+      Message::Eqs => write!(f, "="),
       Message::Num0 => write!(f, "0"),
       Message::Num1 => write!(f, "1"),
       Message::Num2 => write!(f, "2"),
@@ -24,18 +35,7 @@ impl std::fmt::Display for Message {
       Message::Num6 => write!(f, "6"),
       Message::Num7 => write!(f, "7"),
       Message::Num8 => write!(f, "8"),
-      Message::Num9 => write!(f, "9"),
-      Message::LPar => write!(f, "("),
-      Message::RPar => write!(f, ")"),
-      Message::Pow => write!(f, "^"),
-      Message::CE => write!(f, "CE"),
-      Message::BkSp => write!(f, "Back"),
-      Message::Plus => write!(f, "+"),
-      Message::Minus => write!(f, "-"),
-      Message::Mult => write!(f, "*"),
-      Message::Div => write!(f, "/"),
-      Message::Dec => write!(f, "."),
-      Message::Eqs => write!(f, "=")
+      Message::Num9 => write!(f, "9")
     }
   }
 }
@@ -43,8 +43,8 @@ impl std::fmt::Display for Message {
 impl Message {
   pub fn iterator() -> std::slice::Iter<'static, Message> {
     static MESSAGES: [Message; 21] = [
-      CE, Pow, BkSp,
-      LPar, RPar, Div,
+      CE, C, BkSp,
+      Mod, Pow, Div,
       Plus, Minus, Mult,
       Num7, Num8, Num9,
       Num4, Num5,
@@ -70,6 +70,7 @@ const COLOR_BLACK: Color = Color::from_rgb(0, 0, 0);
 const COLOR_LIGHT_GRAY: Color = Color::from_rgb(240, 240, 240);
 const COLOR_LIGHT_PINK: Color = Color::from_rgb(255, 182, 193);
 const COLOR_ORANGE: Color = Color::from_rgb(255, 200, 150);
+const COLOR_YELLOW: Color = Color::from_rgb(255, 239, 186);
 const COLOR_SKY_BLUE: Color = Color::from_rgb(135, 206, 235);
 const COLOR_GREEN: Color = Color::from_rgb(144, 238, 144);
 const COLOR_WHITE: Color = Color::from_rgb(255, 255, 255);
@@ -79,75 +80,9 @@ const BUTTON_FONT_SIZE: i32 = 18;
 const DISPLAY_FONT_SIZE: i32 = 24;
 const FONT_TYPE: enums::Font = enums::Font::HelveticaBold;
 
-fn main() {
-  // APP
-  let mut display_string = String::new();
-
-  let app = app::App::default().with_scheme(fltk::app::Scheme::Gtk);
-
-  let mut win = fltk::window::Window::new(100, 100, WIN_WIDTH, WIN_HEIGHT, "Rustulator").center_screen();
-  let mut display_frame = fltk::frame::Frame::new(20, 10, 320, DISPLAY_HEIGHT, "Display").with_label(&display_string);
-  display_frame.set_pos((win.width() - display_frame.width()) / 2, DISPLAY_MARGIN_TOP); // Center horizontally and set top margin
-  display_frame.set_label_size(DISPLAY_FONT_SIZE);
-  display_frame.set_label_color(COLOR_BLACK);
-  display_frame.set_color(COLOR_WHITE);
-  display_frame.set_frame(FrameType::RFlatBox);
-
-  let (sender, reciever) = app::channel::<Message>();
-
-  // Button Generation
-  let mut butt_count = 0;
-  let mut butts_per_row = 0;
-  let mut y_offset = 50;
-  let button_padding = 10;
-  for button_type in Message::iterator() {
-    let butt_name = button_type.to_string();
-
-    if butt_count % 3 == 0 {
-      butts_per_row = 0;
-      y_offset += BUTT_HEIGHT + button_padding;
-    }
-    let butt_dist = EDGE_DIST + (butts_per_row * (BUTT_WIDTH + button_padding));
-    let mut butt = Button::new(butt_dist, y_offset, BUTT_WIDTH, BUTT_HEIGHT, &*butt_name);
-    butt.set_label_size(BUTTON_FONT_SIZE);
-    butt.set_frame(FrameType::RFlatBox);
-    butt.set_color(match button_type {
-      Message::CE => COLOR_LIGHT_PINK,
-      Message::BkSp => COLOR_ORANGE,
-      Message::Eqs => COLOR_GREEN,
-      _ => COLOR_LIGHT_GRAY,
-    });
-    butt.set_label_color(COLOR_BLACK);
-    butt.set_label_font(FONT_TYPE);
-
-    let orig_color = butt.color();
-    butt.handle(move |b, evt| match evt {
-      Event::Enter => {
-        b.set_color(COLOR_SKY_BLUE);
-        b.set_label_color(COLOR_WHITE);
-        b.redraw();
-        true
-      }
-      Event::Leave => {
-        b.set_color(orig_color);
-        b.set_label_color(COLOR_BLACK);
-        b.redraw();
-        true
-      }
-      Event::Push => {
-        sender.send(*button_type);
-        true
-      }
-      _ => false,
-    });
-
-    butts_per_row += 1;
-    butt_count += 1;
-  }
-
-  win.end();
-  win.show();
-  win.handle(move |_, evt| match evt {
+// EVENT HANDLERS
+fn handle_keyboard_event_closure(sender: app::Sender<Message>) -> impl FnMut(&mut fltk::window::DoubleWindow, fltk::enums::Event) -> bool {
+  move |_, evt| match evt {
     Event::KeyDown => {
       let key = app::event_key();
       let text = app::event_text();
@@ -156,8 +91,7 @@ fn main() {
         enums::Key::Enter => sender.send(Eqs),
         _ => {
           match text.as_str() {
-            "(" => sender.send(Message::LPar),
-            ")" => sender.send(Message::RPar),
+            "%" => sender.send(Message::Mod),
             "." => sender.send(Message::Dec),
             "+" => sender.send(Message::Plus),
             "-" => sender.send(Message::Minus),
@@ -182,22 +116,155 @@ fn main() {
       true
     }
     _ => false,
-  });
+  }
+}
+
+fn handle_button_event_closure(sender: app::Sender<Message>, button_type: Message, orig_color: Color) -> impl FnMut(&mut Button, Event) -> bool {
+  move |b, evt| match evt {
+    Event::Enter => {
+      b.set_color(COLOR_SKY_BLUE);
+      b.set_label_color(COLOR_WHITE);
+      b.redraw();
+      true
+    }
+    Event::Leave => {
+      b.set_color(orig_color);
+      b.set_label_color(COLOR_BLACK);
+      b.redraw();
+      true
+    }
+    Event::Push => {
+      sender.send(button_type);
+      true
+    }
+    _ => false,
+  }
+}
+
+fn main() {
+  // State
+  let mut display_string = String::new();
+  let mut previous_string = String::new();
+  let mut opperation = String::new();
+  let mut solution_cache = String::new();
+
+  // Emitter
+  let (sender, reciever) = app::channel::<Message>();
+
+  // App and Body
+  let app = app::App::default();
+  let mut win = fltk::window::DoubleWindow::new(100, 100, WIN_WIDTH, WIN_HEIGHT, "Rustulator").center_screen();
+
+  // Calculator Screen
+  let mut display_frame = fltk::frame::Frame::new(20, 10, 320, DISPLAY_HEIGHT, "Display").with_label(&display_string);
+  display_frame.set_pos((win.width() - display_frame.width()) / 2, DISPLAY_MARGIN_TOP); // Center horizontally and set top margin
+  display_frame.set_label_size(DISPLAY_FONT_SIZE);
+  display_frame.set_label_color(COLOR_BLACK);
+  display_frame.set_color(COLOR_WHITE);
+  display_frame.set_frame(FrameType::RFlatBox);
+
+  // Button Generation
+  let mut butt_count = 0;
+  let mut butts_per_row = 0;
+  let mut y_offset = 50;
+  let button_padding = 10;
+
+  for button_type in Message::iterator() {
+    let butt_name = button_type.to_string();
+
+    if butt_count % 3 == 0 {
+      butts_per_row = 0;
+      y_offset += BUTT_HEIGHT + button_padding;
+    }
+    let butt_dist = EDGE_DIST + (butts_per_row * (BUTT_WIDTH + button_padding));
+    let mut butt = Button::new(butt_dist, y_offset, BUTT_WIDTH, BUTT_HEIGHT, &*butt_name);
+    butt.set_label_size(BUTTON_FONT_SIZE);
+    butt.set_frame(FrameType::RFlatBox);
+    butt.set_color(match button_type {
+      Message::CE => COLOR_LIGHT_PINK,
+      Message::C => COLOR_YELLOW,
+      Message::BkSp => COLOR_ORANGE,
+      Message::Eqs => COLOR_GREEN,
+      _ => COLOR_LIGHT_GRAY,
+    });
+    butt.set_label_color(COLOR_BLACK);
+    butt.set_label_font(FONT_TYPE);
+
+    let orig_color = butt.color();
+    butt.handle(handle_button_event_closure(sender, *button_type, orig_color));
+
+    butts_per_row += 1;
+    butt_count += 1;
+  }
+
+  win.end();
+  win.show();
+  win.handle(handle_keyboard_event_closure(sender));
 
   while app.wait() {
     if let Some(msg) = reciever.recv() {
       match msg {
         BkSp => {
           display_string.pop();
-          display_frame.set_label(&display_string);
+          display_frame.set_label(&display_string.clone());
         }
         CE => {
+          previous_string.clear();
+          display_string.clear();
+          opperation.clear();
+          solution_cache.clear();
+          display_frame.set_label(&display_string);
+        }
+        C => {
           display_string.clear();
           display_frame.set_label(&display_string);
         }
+        Div | Minus | Mod | Mult | Plus | Pow => {
+          if !display_string.is_empty() {
+            previous_string = display_string.clone();
+            display_string.clear();
+            opperation = msg.to_string();
+            display_frame.set_label(&display_string);
+          }
+          if !solution_cache.is_empty() {
+            previous_string = solution_cache.clone();
+            solution_cache.clear();
+            display_string.clear();
+            opperation = msg.to_string();
+            display_frame.set_label(&display_string);
+          }
+        }
+        Eqs => {
+          if !previous_string.is_empty() {
+            let pre_op: f64 = previous_string.parse().unwrap(); 
+            let post_op: f64 = display_string.parse().unwrap();
+            
+            let solution = match opperation.as_str() {
+              "+" => pre_op + post_op,
+              "-" => pre_op - post_op,
+              "*" => pre_op * post_op,
+              "/" => pre_op / post_op,
+              "^" =>  f64::powf(pre_op, post_op),
+              "%" => pre_op % post_op,
+              _ => post_op,
+            };
+            display_string = solution.to_string();
+            display_frame.set_label(&display_string);
+            opperation.clear();
+            solution_cache = display_string.clone();
+          }
+        }
         _ => {
-          display_string.push_str(&msg.to_string());
-          display_frame.set_label(&display_string);
+          if !solution_cache.is_empty() {
+            previous_string.clear();
+            display_string.clear();
+            opperation.clear();
+            solution_cache.clear();
+            display_frame.set_label(&display_string);
+          } else {
+            display_string.push_str(&msg.to_string());
+            display_frame.set_label(&display_string);
+          }
         }
       }
     }
